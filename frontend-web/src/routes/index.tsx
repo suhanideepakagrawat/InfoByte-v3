4e7cfb66625134dda040e8f90c8bc987fac5c2ed
suhaniagrawat <simplysuhani112@gmail.com>
2026-07-22 17:15:38 +0530

    correcting the api base url

--- frontend-web/src/routes/index.tsx
+++ frontend-web/src/routes/index.tsx
@@ -18,7 +18,7 @@ export const Route = createFileRoute("/")({
 });
 
 const API_BASE =
-  "http://127.0.0.1:8000/api";
+  "https://infobyte-v3.onrender.com/api";
 
 const ALL_TAXONOMY_INTENTS = [
   "technical_code", "technical_oracle", "discussion_social",
